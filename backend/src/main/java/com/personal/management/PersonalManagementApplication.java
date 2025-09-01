f43f4e2ad7d15492cf2695105c59398824f03d78
gzhennaxia <gzhennaxia@gmail.com>
2025-09-01 17:51:18 +0800

    字典方案实现

--- backend/src/main/java/com/personal/management/PersonalManagementApplication.java
+++ backend/src/main/java/com/personal/management/PersonalManagementApplication.java
@@ -2,8 +2,10 @@ package com.personal.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PersonalManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonalManagementApplication.class, args);
