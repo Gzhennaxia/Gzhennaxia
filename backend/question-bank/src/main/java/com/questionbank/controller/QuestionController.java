074f5c15faa49dbef197d16d89329d3022634034
li.bo <li.bo@lesoon.com>
2025-09-15 20:13:43 +0800

    update

--- backend/question-bank/src/main/java/com/questionbank/controller/QuestionController.java
+++ backend/question-bank/src/main/java/com/questionbank/controller/QuestionController.java
@@ -5,9 +5,7 @@ import com.questionbank.entity.QuestionImage;
 import com.questionbank.entity.QuestionType;
 import com.questionbank.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,10 +38,10 @@ public class QuestionController {
      */
     @GetMapping
     public ResponseEntity<Page<Question>> getAllQuestions(
-            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Question> questions = questionService.getAllQuestions(pageable);
+        Page<Question> pageRequest = new Page<>(page, size);
+        Page<Question> questions = questionService.getAllQuestions(pageRequest);
         return ResponseEntity.ok(questions);
     }
     
