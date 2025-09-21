7bb4d797acdf8c7389c109fae41af5c452d86a93
li.bo <li.bo@lesoon.com>
2025-09-21 18:18:57 +0800

    update

--- backend/common/src/main/java/com/gzhennaxia/common/controller/IBaseController.java
+++ backend/common/src/main/java/com/gzhennaxia/common/controller/IBaseController.java
@@ -5,6 +5,11 @@ import com.gzhennaxia.common.pojo.converter.PageConverter;
 import com.gzhennaxia.common.pojo.dto.PageDto;
 import com.gzhennaxia.common.pojo.request.PageRequest;
 import com.gzhennaxia.common.service.IBaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +17,20 @@ public abstract class IBaseController<T> {
 
     protected abstract IBaseService<T> getBaseService();
 
-    // dictId
+    /**
+     * 分页查询接口
+     *
+     * @param pageRequest 分页请求参数，包含分页信息和查询条件
+     * @return 分页查询结果，包含数据列表和分页信息
+     */
+    @Operation(summary = "分页查询", description = "根据分页参数和查询条件进行分页查询")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "查询成功",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = IPage.class))),
+            @ApiResponse(responseCode = "400", description = "请求参数错误"),
+            @ApiResponse(responseCode = "500", description = "服务器内部错误")
+    })
     @PostMapping("/page")
     public IPage<T> page(@RequestBody PageRequest<T> pageRequest) {
         PageDto<T> pageDto = PageConverter.convert(pageRequest);
