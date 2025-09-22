af125e4c66acee8e4e3f5a813c9243344f162a37
li.bo <li.bo@lesoon.com>
2025-09-22 23:54:24 +0800

    update

---
+++ backend/civil-servant-service/src/main/java/com/gzhennaxia/civilservantservice/CivilServantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gzhennaxia.civilservantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilServantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivilServantServiceApplication.class, args);
+    }
+
+}
