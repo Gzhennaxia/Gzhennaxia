f3581c05b08c6619f70fa6be85acc59b61c66d29
gzhennaxia <gzhennaxia@gmail.com>
2025-10-19 20:37:57 +0800

    update

--- backend/question-bank-service/src/main/java/com/gzhennaxia/question/bank/service/VideoService.java
+++ backend/question-bank-service/src/main/java/com/gzhennaxia/question/bank/service/VideoService.java
@@ -205,7 +205,7 @@ public class VideoService {
             // 传入FFmpeg的bin目录（包含所有工具）
             String ffmpegDir = "D:\\SOFTERWARE\\ffmpeg-7.1.1-full_build\\bin";
             // 视频路径
-            String videoPath = "D:\\SOFTERWARE\\BBDown_1.6.3_20240814_win-x64\\ZXM-XTK\\[P10]10 逻辑判断-论证逻辑4-日常结论、解释原因题.mp4";
+            String videoPath = "D:\\SOFTERWARE\\BBDown_1.6.3_20240814_win-x64\\ZXM-XTK\\[P11]11类比推理1-概述、概念关系.mp4";
             // 输出目录
             String outputDir = "D:\\SOFTERWARE\\BBDown_1.6.3_20240814_win-x64\\ZXM-XTK\\screenshots";
             // 截图间隔
